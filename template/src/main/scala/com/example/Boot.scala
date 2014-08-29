076e9e78026cc6130bd9e7e7a794e4e8a8446542
Juan Pedro <juanpmorest@gmail.com>
2014-08-29 14:08:23 +0200

    App Spray Boot changed to use openshift environment variables

--- template/src/main/scala/com/example/Boot.scala
+++ template/src/main/scala/com/example/Boot.scala
@@ -16,6 +16,6 @@ object Boot extends App {
   val service = system.actorOf(Props[MyServiceActor], "demo-service")
 
   implicit val timeout = Timeout(5.seconds)
-  // start a new HTTP server on port 8080 with our service actor as the handler
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
+  // start a new HTTP server on port 8080 with our service actor as the handler  
+  IO(Http) ! Http.Bind(service, interface = System.getenv("OPENSHIFT_SCALA_IP"), port = System.getenv("OPENSHIFT_SCALA_PORT").toInt)
 }
