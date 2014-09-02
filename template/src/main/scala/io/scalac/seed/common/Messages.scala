e3e71ca2d4dc35a664789759e93923fe98f9aaf7
Juan Pedro <juanpmorest@gmail.com>
2014-09-02 17:54:24 +0200

    without template

--- template/src/main/scala/io/scalac/seed/common/Messages.scala
+++
@@ -1,5 +0,0 @@
-package io.scalac.seed.common
-
-case class Error(message: String)
-
-case class Acknowledge(id: String)
--- template/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-${OPENSHIFT_DATA_DIR}/sbt/bin/sbt run io.scalac.seed.Boot
-
